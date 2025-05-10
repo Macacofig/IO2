9af087090535f358a3be1bb7737f87f67a183795
omkrfig <omkrfig@gmail.com>
2025-05-10 18:13:16 -0400

    Initial Back

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SERVER_PORT } from './config/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get(SERVER_PORT);
+  app.enableCors({
+    origin: 'http://localhost:4200', // Angular local
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+  await app.listen(port);
 }
 bootstrap();
+
