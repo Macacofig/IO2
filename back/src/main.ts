0277bb809613a7898cb0f578fe65acddd58ecb50
omkrfig <omkrfig@gmail.com>
2025-06-03 22:39:22 -0400

    env para render

--- back/src/main.ts
+++ back/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     credentials: true // Si necesitas cookies o cabeceras autorizadas
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 
